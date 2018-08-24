d83a75eace4c7e9e156842749f7e7cf00923ab8d
OliTram <42636649+OliTram@users.noreply.github.com>
2018-08-24 10:27:41 +0200

    fix wording

--- display.java
+++ display.java
@@ -4,7 +4,7 @@ public class display {
 
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in); 
-		System.out.println("Enteryour name : ");
+		System.out.println("Enter your name : ");
 		String name = reader.next();
 		System.out.println(" your name is : " + name);
 	}
