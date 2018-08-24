24038a1042f7a9393a41bc66376e04be66fcb021
oml <oml@3ds.com>
2018-08-24 10:26:23 +0200

    display name

---
+++ display.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class display {
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in); 
+		System.out.println("Enteryour name : ");
+		String name = reader.next();
+		System.out.println(" your name is : " + name);
+	}
+
+}
+
