2b784744df53796d3b7b4ba74e4b3190ce8eafa3
oml <oml@3ds.com>
2018-08-24 10:35:42 +0200

    nickname adding

--- display.java
+++ display.java
@@ -6,10 +6,13 @@ public class display {
 		Scanner reader = new Scanner(System.in); 
 		System.out.println("Enter your name : ");
 		String name = reader.next();
+		System.out.println("Enter your nickname : ");
+                String nickname = reader.next();
 		System.out.println("Enter your address : ");
                 String address = reader.next();
 
-		System.out.println(" your name is : " + name +" and your address" + address);
+
+		System.out.println(" your name is : " + name + " your nickname is : " + nickname + " and your address" + address);
 	}
 
 }
